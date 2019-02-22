1db88fe2e2fd3af5e989e1bb7ceade3368c0b113
Emerson Cedeno <emerson.cedeno@logicstudio.net>
2019-02-22 17:22:21 -0500

    CLASE 14 - Creación de la clase Curso e incorporación al proyecto

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace CorEscuela.Entidades
 {
     public class Curso
     {
-        
+        public string UniqueId { get; private set; }
+        public string Nombre { get; set; }
+        public TiposJornada Jornada { get; set; }
+
+        public Curso() => UniqueId = Guid.NewGuid().ToString();
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,11 +10,22 @@ namespace etapa1
             var escuela = new Escuela("Platzi Academy", 2019, TiposEscuela.Secundaria, 
                 ciudad:"David"
             );
-            // escuela.Pais = "Panamá";
-            // escuela.Ciudad = "Panamá";
-            // escuela.TipoEscuela = TiposEscuela.Primaria;
+            
+            var curso1 = new Curso() {
+                Nombre = "101"
+            };
+            var curso2 = new Curso() {
+                Nombre = "201"
+            };
+            var curso3 = new Curso() {
+                Nombre = "301"
+            };
 
             Console.WriteLine(escuela);
+            System.Console.WriteLine("==========");
+            System.Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
+            System.Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+            System.Console.WriteLine($"{curso3.Nombre}, {curso3.UniqueId}");
         }
     }
 }
